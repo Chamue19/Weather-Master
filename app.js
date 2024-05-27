8d50f724ce3a23360de9fe8c129bc1dde16c6fc6
Chamue19 <115542082+Chamue19@users.noreply.github.com>
2024-05-27 18:15:32 +0530

    update app.js

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const wheather = require('./utils/wheather.js');
  //express() aniwryen mek gnn one
  const app = express();
 
+ app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+})
+app.get('/',(req,res)=>{
+    res.send('hello express');
+})
+app.get('/API',(req,res)=>{
+    res.send('API page');
+})
+app.get('/wheather',(req,res)=>{
+    console.log(req.query.location); //
+})
+app.get('*',(req,res)=>{
+    res.send('404 page not found');
+})
   
 // geo('piduruwella')
 // .then((data)=>{
@@ -24,6 +39,3 @@ const wheather = require('./utils/wheather.js');
 //     console.log(error);
 // })
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
-})
